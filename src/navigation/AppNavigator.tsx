f66176885bbf8b95913c6ea8a4f81c35bc65119c
YeonLog <jyb0422@naver.com>
2024-07-08 20:24:43 +0900

    [feat] 네비게이터 구현
    AppNavigator 및 BottomTabNavigator 개발

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,32 @@
+// src/navigation/AppNavigator.tsx
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BottomTabNavigator from './BottomTabNavigator';
+import Home from '../screens/Home/homescreen';
+import Search from '../screens/Search/searchscreen';
+import My from '../screens/MY/myscreen';
+import Create from '../screens/Create/createscreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabNavigator}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="My" component={My} />
+        <Stack.Screen name="Create" component={Create} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ src/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home/homescreen';
+import Search from '../screens/Search/searchscreen';
+import My from '../screens/MY/myscreen';
+import Create from '../screens/Create/createscreen';
+
+const homeIcon = require('../img/home.png');
+const searchIcon = require('../img/search.png');
+const myIcon = require('../img/my.png');
+const createIcon = require('../img/create.png');
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconSource;
+
+          if (route.name === 'Home') {
+            iconSource = homeIcon;
+          } else if (route.name === 'Search') {
+            iconSource = searchIcon;
+          } else if (route.name === 'My') {
+            iconSource = myIcon;
+          } else if (route.name === 'Create') {
+            iconSource = createIcon;
+          }
+
+          return (
+            <Image
+              source={iconSource}
+              style={{width: size, height: size, tintColor: color}}
+            />
+          );
+        },
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Create" component={Create} />
+      <Tab.Screen name="My" component={My} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
